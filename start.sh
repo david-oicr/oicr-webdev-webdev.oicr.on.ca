546915dd8b7bb07b36d6966fa8794ff3fdca8d48
David Chi <david.chi@oicr.on.ca>
2019-01-15 10:42:04 -0500

    added carelynn page

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export JEKYLL_VERSION=3.5
 set -x
-docker run --name=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_CONTAINER -e VIRTUAL_HOST=webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_HOST -e CERT_NAME=webdevff0b3218aaad11e7abc4cec278b6b50a_ENV_DOMAIN --rm --link webdevff0b3218aaad11e7abc4cec278b6b50a_API_CONTAINER:webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST -v $(pwd):/usr/src/app -P --expose 4000 -ti jekyll:docker bash -c 'cd /usr/src/app; sudo bundle update; jekyll serve -d _site --watch --host=0.0.0.0 --port webdevff0b3218aaad11e7abc4cec278b6b50a_JEKYLL_PORT'
+IP=`ifconfig eth0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+nohup docker run --name=webdevcr -e VIRTUAL_HOST=webdevcr.david.vm -e CERT_NAME=david.vm --rm --add-host webdevcr-api.david.vm:127.0.0.1 -v $(pwd):/usr/src/app -P --expose 4000 jekyll:docker bash -c 'cd /usr/src/app; sudo bundle update; sudo jekyll serve -d _site --watch --host=0.0.0.0 --port 4000' > ../logs/webdevcr.log
